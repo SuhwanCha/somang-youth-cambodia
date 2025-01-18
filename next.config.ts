b285c25d5e7956e1432d0baca02c3cf802cff638
SuhwanCha <suhwan.cha@ezar.co.kr>
2025-01-18 18:25:20 +0900

    chore: fix static files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cambodia.suhwan.me"],
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
